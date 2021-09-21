ad655072b25e4a66ff7a0ed7b71be2f19abd1b18
Ben <2432411j@student.gla.ac.uk>
2021-09-21 23:26:34 +0100

    Added player images and paths

--- src/controller.java
+++ src/controller.java
@@ -2,26 +2,26 @@
 public class controller {
 	
 	public static void createDrivers() {
-		driver verstappen = new driver(33,"Max Verstappen",23,17,52,99,99,99,99,"description","imagepath");
-		driver hamilton = new driver(44,"Lewis Hamilton",36,99,175,99,99,99,99,"description","imagepath");
-		driver bottas = new driver(77,"Valtteri Bottas",32,9,64,99,99,99,99,"description","imagepath");
-		driver norris = new driver(4,"Lando Norris",21,0,5,99,99,99,99,"description","imagepath");
-		driver perez = new driver(11,"Sergio Perez",31,2,12,99,99,99,99,"description","imagepath");
-		driver leclerc = new driver(16,"Charles Leclerc",23,2,13,99,99,99,99,"description","imagepath");
-		driver sainz = new driver(55,"Carlos Sainz",27,0,4,99,99,99,99,"description","imagepath");
-		driver ricciardo = new driver(3,"Daniel Ricciardo",32,8,32,99,99,99,99,"description","imagepath");
-		driver gasly = new driver(10,"Pierre Gasly",25,1,3,99,99,99,99,"description","imagepath");
-		driver alonso = new driver(14,"Fernando Alonso",40,32,97,99,99,99,99,"description","imagepath");
-		driver ocon = new driver(31,"Esteban Ocon",25,1,2,99,99,99,99,"description","imagepath");
-		driver vettel = new driver(5,"Sebastian Vettel",34,53,122,99,99,99,99,"description","imagepath");
-		driver stroll = new driver(18,"Lawrence Stroll",22,0,3,99,99,99,99,"description","imagepath");
-		driver tsunoda = new driver(22,"Yuki Tsunoda",21,0,0,99,99,99,99,"description","imagepath");
-		driver russell = new driver(63,"George Russell",23,0,1,99,99,99,99,"description","imagepath");
-		driver latifi = new driver(6,"Nicholas Latifi",26,0,0,99,99,99,99,"description","imagepath");
-		driver raikkonen = new driver(7,"Kimi Raikkonen",41,21,103,99,99,99,99,"description","imagepath");
-		driver giovinazzi = new driver(99,"Antonio Giovinazzi",27,0,0,99,99,99,99,"description","imagepath");
-		driver schumacherJr = new driver(47,"Mick Schumacher",22,0,0,99,99,99,99,"description","imagepath");
-		driver mazepin = new driver(9,"Nikita Mazepin",22,0,0,99,99,99,99,"description","imagepath");
+		driver verstappen = new driver(33,"Max Verstappen",23,17,52,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/verstappen.png");
+		driver hamilton = new driver(44,"Lewis Hamilton",36,99,175,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/hamilton.png");
+		driver bottas = new driver(77,"Valtteri Bottas",32,9,64,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/bottas.png");
+		driver norris = new driver(4,"Lando Norris",21,0,5,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/norris.png");
+		driver perez = new driver(11,"Sergio Perez",31,2,12,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/perez.png");
+		driver leclerc = new driver(16,"Charles Leclerc",23,2,13,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/leclerc.png");
+		driver sainz = new driver(55,"Carlos Sainz",27,0,4,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/sainz.png");
+		driver ricciardo = new driver(3,"Daniel Ricciardo",32,8,32,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/ricciardo.png");
+		driver gasly = new driver(10,"Pierre Gasly",25,1,3,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/gasly.png");
+		driver alonso = new driver(14,"Fernando Alonso",40,32,97,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/alonso.png");
+		driver ocon = new driver(31,"Esteban Ocon",25,1,2,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/Ocon.png");
+		driver vettel = new driver(5,"Sebastian Vettel",34,53,122,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/vettel.png");
+		driver stroll = new driver(18,"Lawrence Stroll",22,0,3,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/stroll.png");
+		driver tsunoda = new driver(22,"Yuki Tsunoda",21,0,0,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/tsunoda.png");
+		driver russell = new driver(63,"George Russell",23,0,1,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/russell.png");
+		driver latifi = new driver(6,"Nicholas Latifi",26,0,0,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/latifi.png");
+		driver raikkonen = new driver(7,"Kimi Raikkonen",41,21,103,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/raikkonen.png");
+		driver giovinazzi = new driver(99,"Antonio Giovinazzi",27,0,0,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/giovinazzi.png");
+		driver schumacherJr = new driver(47,"Mick Schumacher",22,0,0,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/schumacherJr.png");
+		driver mazepin = new driver(9,"Nikita Mazepin",22,0,0,99,99,99,99,"description","/Users/benjohnston/F1TopTrumps/imgs/mazepin.png");
 		driver[] drivers={verstappen,hamilton,bottas,norris,perez,leclerc,sainz,ricciardo,gasly,alonso,ocon,vettel,stroll,tsunoda,russell,latifi,raikkonen,giovinazzi,schumacherJr,mazepin};
 	}
 	
--- src/driver.java
+++ src/driver.java
@@ -27,6 +27,94 @@ public class driver {
 		this.description = description;
 		this.imagePath = imagePath;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	public int getPodiums() {
+		return podiums;
+	}
+
+	public void setPodiums(int podiums) {
+		this.podiums = podiums;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public int getRacecraft() {
+		return racecraft;
+	}
+
+	public void setRacecraft(int racecraft) {
+		this.racecraft = racecraft;
+	}
+
+	public int getReliability() {
+		return reliability;
+	}
+
+	public void setReliability(int reliability) {
+		this.reliability = reliability;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
 	
 	
 }
